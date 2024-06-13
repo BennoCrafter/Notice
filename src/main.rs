6d6b1f6f808517bfa85a133fc1b5e69af51f8aad
Benno <bennocrafterdev@googlemail.com>
2024-06-13 18:07:12 +0200

    added some stuff. ye

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 use std::rc::Rc;
+use lazy_static::lazy_static;
+use std::sync::{Mutex, Arc};
 
 /* fn read_file_content(file_path: &str) -> io::Result<String> {
     // Open the file
@@ -31,13 +33,17 @@ use std::rc::Rc;
     Ok(contents)
 } */
 
+lazy_static! {
+    static ref CURRENT_CURSOR_LINE: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+}
+
 struct EditorStyling {
     pub style: Rc<dyn Styling>,
 }
 
 impl Styling for EditorStyling {
     fn id(&self) -> u64 {
-        0
+        self.style.id()
     }
 
     fn apply_attr_styles(
@@ -45,13 +51,13 @@ impl Styling for EditorStyling {
         _edid: EditorId,
         _style: &EditorStyle,
         line: usize,
-        _default: Attrs,
+        _default: Attrs, 
         attrs: &mut AttrsList,
     ) {
         attrs.clear_spans();
-        // todo this can be improved
+        println!("executed style on line: {}", line);
         if line == 0 {
-            let attr = Attrs::new().color(Color::WHITE).font_size(20 as f32);
+            let attr = Attrs::new().color(Color::WHITE).font_size(20.0);
             attrs.add_span(Range { start: 0, end: 10 }, attr);
         } else if line == 2 {
             let attr = Attrs::new().color(Color::WHITE).weight(Weight::BOLD);
@@ -66,16 +72,64 @@ impl Styling for EditorStyling {
         line: usize,
         layout_line: &mut TextLayoutLine,
     ) {
-        self.style
-            .apply_layout_styles(edid, style, line, layout_line)
+        self.style.apply_layout_styles(edid, style, line, layout_line)
     }
 
     fn paint_caret(&self, edid: EditorId, line: usize) -> bool {
+        // println!("Current cursor line: {}", line);
+        *CURRENT_CURSOR_LINE.lock().unwrap() = line;
         self.style.paint_caret(edid, line)
+    }
+    
+    fn font_size(&self, _edid: EditorId, _line: usize) -> usize {
+        16
+    }
+    
+    fn line_height(&self, edid: EditorId, line: usize) -> f32 {
+        let font_size = self.font_size(edid, line) as f32;
+        (1.5 * font_size).round().max(font_size)
+    }
+    
+    fn font_family(&self, _edid: EditorId, _line: usize) -> std::borrow::Cow<[FamilyOwned]> {
+        std::borrow::Cow::Borrowed(&[FamilyOwned::SansSerif])
+    }
+    
+    fn weight(&self, _edid: EditorId, _line: usize) -> Weight {
+        Weight::NORMAL
+    }
+    
+    fn italic_style(&self, _edid: EditorId, _line: usize) -> floem::cosmic_text::Style {
+        floem::cosmic_text::Style::Normal
+    }
+    
+    fn stretch(&self, _edid: EditorId, _line: usize) -> Stretch {
+        Stretch::Normal
+    }
+    
+    fn indent_line(&self, _edid: EditorId, line: usize, _line_content: &str) -> usize {
+        line
+    }
+    
+    fn tab_width(&self, _edid: EditorId, _line: usize) -> usize {
+        4
+    }
+    
+    fn atomic_soft_tabs(&self, _edid: EditorId, _line: usize) -> bool {
+        false
     }
 }
 
+lazy_static! {
+    static ref SKIP_INIT_ATTRS_LOAD: Mutex<bool> = Mutex::new(true);
+}
+
+fn toggle_skip_init_attrs_load(new_value: bool) {
+    let mut skip_init = SKIP_INIT_ATTRS_LOAD.lock().unwrap();
+    *skip_init = new_value;
+}
+
 fn app_view() -> impl IntoView {
+
     let global_style = SimpleStylingBuilder::default()
         .wrap(WrapMethod::None)
         .font_family(vec![
@@ -88,35 +142,33 @@ fn app_view() -> impl IntoView {
     let style = EditorStyling {
         style: Rc::new(global_style),
     };
-
     let file_content =
-        "Big text! Now normal text!\nNothing special in this line\nBut now bold! normal again
-    ";
-    let mut editor = text_editor(file_content);
+        "Big text! Now normal text!\nNothing special in this line\nBut now bold! normal again";
 
-    editor = editor
+    let editor = text_editor(file_content);
+
+    let editor = editor
         .styling(style)
         .editor_style(default_dark_color)
         .editor_style(move |s| s.hide_gutter(true))
-        .style(|s| s.size_full().padding(20.0));
+        .style(|s| s.size_full().padding(20.0))
+        .update(move |update| {
+            if let Some(editor) = &update.editor {
+                println!("Update: {}", editor.doc().text());
+            } else {
+                println!("No editor available to provide an update.");
+            }
+        });
 
     let doc = editor.doc();
-    editor = editor.update(move |update| {
-        if let Some(editor) = &update.editor {
-            println!("Update: {}", editor.doc().text());
-        } else {
-            println!("No editor available to provide an update.");
-        }
-    });
-
     let view = stack((editor,)).style(|s| s.size_full().flex_col().items_center().justify_center());
-
     let id = view.id();
     view.on_key_up(Key::Named(NamedKey::F11), Modifiers::empty(), move |_| {
         id.inspect()
     })
+
 }
 
 fn main() {
-    floem::launch(app_view)
+    floem::launch(app_view);
 }
